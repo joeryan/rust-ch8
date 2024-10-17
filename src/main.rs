84a851f07c108280efa8a75a1206763318a4c1d6
Joe Ryan <joe@clanryan.us>
2024-10-16 20:15:37 -0400

    completed mean/mode calculator of random integers

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,13 @@ fn main() {
     // Rust book Chapter 8
 
     use rand::Rng;
+    use std::collections::HashMap;
+
     let mut rand_num = rand::thread_rng();
 
     // generate random list of integers
     let mut int_list: Vec<i32> = Vec::new();
-    for _i in 0..10 {
+    for _i in 0..100 {
         int_list.push(rand_num.gen_range(0..10))
     }
 
@@ -17,9 +19,9 @@ fn main() {
     int_list.sort();
     // print list
     for i in &int_list {
-        print!("{i} ");
+        print!("{i}\n");
     }
-    println!("\n----");
+    // println!("\n----");
     // calculate median - middle value
     let list_len = int_list.len();
     let even_odd = list_len % 2;
@@ -34,4 +36,28 @@ fn main() {
     }
 
     // calculate mode - value occurring most often
+    // insert values into hash-map
+    let mut int_map = HashMap::new();
+
+    for num in int_list {
+        let count = int_map.entry(num).or_insert(0);
+        *count += 1;
+    }
+
+    println!("-----\n{int_map:?}");
+    let mut high_count = 0;
+    let mut high_key: Vec<i32> = Vec::new();
+    for (key, value) in &int_map {
+        if *value > high_count {
+            high_count = *value;
+            if high_key.len() > 0 {
+                high_key = vec![*key];
+            } else {
+                high_key.push(*key);
+            }
+        } else if *value == high_count {
+            high_key.push(*key);
+        }
+    } 
+    println!("----\nMode(s): {high_key:?}");
 }
